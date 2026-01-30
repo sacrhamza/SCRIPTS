230fff833f6988bef85eb1b688ab795291ac126c
sacrhamza <sacrhamzaschool@gmail.com>
2026-01-30 15:17:16 +0100

    update my tools

--- tmux.sh
+++ tmux.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 
+web=(nodejs npm)
+
+compilers=(gcc clang g++ clang++)
+
+editors=(noevim vim)
+
+build_tools=(make)
+
+devolop_tools=(python3 go eza yazi curl
+	wget )
+
+package_man=(python3-pip cargo)
+
+shells=(zsh fish)
+
 apps=(
-	eza
-	neovim
-	tmux
-	bash
-	hyperfine
-	zsh
-	fish
-	git
-	gcc
-	clang
-	vim
-	curl
-	python3
-	python3-pip
-	stow
-	nodejs
-	npm
-	rust
-	ffmpeg
-	go
-	zoxide
+	eza neovim tmux bash hyperfine
+	zsh fish git gcc clang vim curl
+	python3 python3-pip stow nodejs
+	npm rust ffmpeg go zoxide
 	bat
 )
 
